327111b70c0527b25dc26014e1dcd839d58fbab2
Vaibhav Bali <vibsbali@gmail.com>
2016-12-09 18:18:08 +1100

    init

---
+++ AlgorithmsPrinceton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SearchingAlgorithms;
+
+namespace AlgorithmsPrinceton
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            var array = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            var searchAlgorithms = new BinarySearch<int>();
+
+            var result = searchAlgorithms.Search(array, 11);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ SearchingAlgorithms/BinarySearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SearchingAlgorithms
+{
+    public class BinarySearch<T> where T : IComparable<T>
+    {
+        public bool Search(T[] items, T value)
+        {
+            var low = 0;
+            var hi = items.Length - 1;
+
+            while (low <= hi)
+            {
+                var mid = (hi - low) / 2 + low;
+
+                if (items[mid].CompareTo(value) == 0)
+                {
+                    return true;
+                }
+
+                if (items[mid].CompareTo(value) > 0)
+                {
+                    hi = mid - 1; //substract - 1 because mid is already checked
+                }
+                else
+                {
+                    low = mid + 1; //Add + 1 because mid is already checked
+                }
+
+            }
+
+            return false;
+        }
+    }
+}
